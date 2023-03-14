596df476315aa1d568c0376d2bfa16361bf15c0f
atyuna <121321846+atyuna@users.noreply.github.com>
2023-03-14 12:00:18 -0400

    Create find_element_by_css_xpath.py

---
+++ find_element_by_css_xpath.py
@@ -0,0 +1,27 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pdb
+
+driver = webdriver.Chrome()
+
+driver.get('http://demostore.supersqa.com/shop/')
+#  EXAMPLE: CSS SELECTOR
+# cart = driver.find_element(By.CSS_SELECTOR, '#site-header-cart > li:nth-child(1) > a')
+# cart.click()
+# my_acc = driver.find_element(By.CSS_SELECTOR,'#site-navigation > div:nth-child(2) > ul > li.page_item.page-item-9')
+# my_acc.click()
+
+# time.sleep(3)
+# driver.quit()
+
+#  EXAMPLE: XPATH
+# NOTE: xpath changes a lot, not recommended to use
+
+cart = driver.find_element(By.XPATH, '//*[@id="site-header-cart"]/li[1]/a')
+time.sleep(3)
+cart.click()
+my_acc = driver.find_element(By.XPATH,'//*[@id="site-navigation"]/div[1]/ul/li[4]/a')
+time.sleep(3)
+my_acc.click()
